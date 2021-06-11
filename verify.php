9c5695aec7aacaace4b38404c781695ec1cb6aea
bigza1210 <bigza1210@hotmail.co.th>
2021-06-11 16:26:48 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -1,5 +1,5 @@
 <?php
-$access_token = 'adPuEVK1ceK3T+vhY/n8rQHCav+hySTEhsJEr7bpBH6MQn7HV+M0ge8BNG6BasJOLw1Jh5Qv8py4OkNtiM4QRDCxFiWbJ37BzwNAm2InSMVSVC+eSRLzeqr5LxvEtnEl0VJkb2pgok5aHeQXO6Db4gdB04t89/1O/w1cDnyilFU=';
+$access_token = 'rJHE9UvM/q1dt71JBbTMTwFqpX4z94DYdWNofcAsskb/xDHa4DAq90Ty7CGPeVyOWCILPAmhUOY4buExHWmrdqxle6M1KAH0shuUVdx1hjEpSR7aaemmDW3UiCPAN5xCeh4Hi+2OyQ0L009lqn5hygdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
